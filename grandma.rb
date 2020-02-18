0a19208c8b0249741004b77dc882ac365709eac4
Alvin Coulson <aaacoulson@gmail.com>
2020-02-18 05:46:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
     return 'HUH?! SPEAK UP, SONNY!'
   elsif string == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif string == 'WHAT DID YOU EAT TODAY?' || string == WHAT?'
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
