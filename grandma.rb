56c6dd14f44212e6c7dde87140775627956a6780
Alvin Coulson <aaacoulson@gmail.com>
2020-02-18 05:43:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  unless string.upcase?
+  if string == 'Hi Nana, how are you?'
     return 'HUH?! SPEAK UP, SONNY!'
   elsif string == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
